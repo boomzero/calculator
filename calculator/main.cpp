b19ce91280f26d000f110163ab0d2715a452e8cd
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2022-08-04 14:14:37 +0800

    basic

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <cctype>
 using namespace std;
 int getPriority(string op){
     if (op=="^") {
@@ -11,6 +12,19 @@ int getPriority(string op){
     }
 }
 int main() {
-    
+    stack<string> s1;
+    stack<string> s2;
+    string expression;
+    cin>>expression;
+    string temp;
+    for (int i=0; i<expression.length(); i++) {
+        if (isdigit(expression[i])) {
+            while (isdigit(expression[i])) {
+                temp.push_back(expression[i]);
+                i++;
+            }
+            s2.push(temp);
+        }
+    }
     return 0;
 }
