c23bb62cc33508091c2d309492e2af60b62c9594
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2022-08-04 13:38:56 +0800

    Initial Commit

---
+++ calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  calculator
+//
+//  Created by 陈琦 on 8/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
