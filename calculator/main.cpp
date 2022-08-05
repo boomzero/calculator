d66337201ea17eb5728d644eee2c12c9c2a85df2
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2022-08-05 13:50:24 +0800

    finish

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <string>
+#include <cmath>
 #include <cctype>
 using namespace std;
 int getPriority(string op){
@@ -61,5 +63,31 @@ int main() {
             be.insert(0, " ");
         s2.pop();
     }
+    stack<string> cs;
+    string temp2;
+    long long val1=0,val2=0;
+    for (int i=0; i<be.length(); i++) {
+        temp2="";
+        if (isdigit(be[i])) {
+            while (be[i]!=' ') {
+                temp2.push_back(be[i]);
+                i++;
+            }
+            cs.push(temp2);
+        }else{
+            val2=stoi(cs.top());
+            cs.pop();
+            val1=stoi(cs.top());
+            cs.pop();
+            if (be[i]=='+') {
+                cs.push(to_string(val1+val2));
+            }else if (be[i]=='-') cs.push(to_string(val1-val2));
+            else if (be[i]=='*') cs.push(to_string(val1*val2));
+            else if (be[i]=='/') cs.push(to_string(val1*val2));
+            else cs.push(to_string(pow(val1, val2)));
+            i++;
+        }
+    }
+    cout<<cs.top()<<endl;
     return 0;
 }
