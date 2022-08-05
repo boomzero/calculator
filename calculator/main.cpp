4163c73dbcb532bf06de12e6fad98973652b7dfb
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2022-08-05 12:29:46 +0800

    convert expression

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -18,13 +18,40 @@ int main() {
     cin>>expression;
     string temp;
     for (int i=0; i<expression.length(); i++) {
-        if (isdigit(expression[i])) {
+        if (isdigit(expression[i])) {//This is a number
             while (isdigit(expression[i])) {
                 temp.push_back(expression[i]);
                 i++;
             }
             s2.push(temp);
+        }else{//This is an operator
+            if (s1.empty()||s1.top()=="(") {
+                temp="";
+                temp.push_back(expression[i]);
+                s1.push(temp);
+            }else{
+                temp="";
+                temp.push_back(expression[i]);
+                if (temp==")") {
+                    while (s1.top()!="(") {
+                        s2.push(s1.top());
+                        s1.pop();
+                    }
+                    s1.pop();
+                }else{
+                    while (!(getPriority(temp)>getPriority(s1.top()))&&(!s1.empty())) {
+                        s2.push(s1.top());
+                        s1.pop();
+                    }
+                s1.push(temp);
+                }
+            }
         }
+    }
+    string be;
+    while (!s2.empty()) {
+        be.insert(0, s2.top);
+        s2.pop();
     }
     return 0;
 }
