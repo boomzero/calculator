2f164d3f5256aedc8a5d52ac38a34726184421d0
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2022-08-06 19:48:28 +0800

    fix pop when s1 is empty

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -30,7 +30,7 @@ int main() {
             s2.push(temp);
         }else{//This is an operator
             if (s1.empty()||(s1.top()=="(")) {
-                if ((expression[i]!=')')) {
+                if ((expression[i]!=')')&&!s1.empty()) {
                     s1.pop();
                 }else{
                     temp="";
