4ca9b91d6c6df178f1da3a3e888301732824abd1
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2022-08-25 13:40:16 +0000

    format code

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -2,11 +2,9 @@
 This program is free software: you can redistribute it and/or modify it under the terms of
 the GNU General Public License as published by the Free Software Foundation, either
 version 3 of the License, or (at your option) any later version.
-
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License along with this program.
 If not, see <https://www.gnu.org/licenses/>.
 */
@@ -18,71 +16,75 @@ If not, see <https://www.gnu.org/licenses/>.
 #include <cstdlib>
 #include <sstream>
 #include <iomanip>
+
 using namespace std;
-string ldtos(long double val){
+
+string ldtos(long double val) {
     std::stringstream ss;
     ss << std::setprecision(15) << val;
     return ss.str();
 }
-int getPriority(string op){
-    if (op=="^") {
+
+int getPriority(string op) {
+    if (op == "^") {
         return 3;
-    }else if (op=="*"||op=="/"){
+    } else if (op == "*" || op == "/") {
         return 2;
-    }else if (op=="+"||op=="-"){
+    } else if (op == "+" || op == "-") {
         return 1;
     }
     return 0;
 }
+
 int main() {
     stack<string> s1;
     stack<string> s2;
     string expression;
-    cin>>expression;
-    if (expression[0]=='-'||expression[0]=='+') {
+    cin >> expression;
+    if (expression[0] == '-' || expression[0] == '+') {
         expression.insert(0, "0");
     }
-    for (int i=1; i<expression.length(); i++) {
-        if ((expression[i]=='-'||expression[0]=='+')&&expression[i-1]=='(') {
+    for (int i = 1; i < expression.length(); i++) {
+        if ((expression[i] == '-' || expression[0] == '+') && expression[i - 1] == '(') {
             expression.insert(i, "0");
         }
     }
     string temp;
-    for (int i=0; i<expression.length(); i++) {
-        temp="";
+    for (int i = 0; i < expression.length(); i++) {
+        temp = "";
         if (isdigit(expression[i])) {
-            while (isdigit(expression[i])||expression[i]=='.') {
+            while (isdigit(expression[i]) || expression[i] == '.') {
                 temp.push_back(expression[i]);
                 i++;
             }
             i--;
             s2.push(temp);
-        }else{
-            if (s1.empty()||(s1.top()=="(")) {
-                if (!s1.empty()&&(expression[i]==')')&&s1.top()=="(") {
+        } else {
+            if (s1.empty() || (s1.top() == "(")) {
+                if (!s1.empty() && (expression[i] == ')') && s1.top() == "(") {
                     s1.pop();
-                }else{
-                    temp="";
+                } else {
+                    temp = "";
                     temp.push_back(expression[i]);
                     s1.push(temp);
                 }
-            }else{
-                temp="";
+            } else {
+                temp = "";
                 temp.push_back(expression[i]);
-                if (temp=="(") {
+                if (temp == "(") {
                     s1.push("(");
-                }else if (temp==")") {
-                    while (s1.top()!="(") {
+                } else if (temp == ")") {
+                    while (s1.top() != "(") {
                         s2.push(s1.top());
                         s1.pop();
                     }
                     s1.pop();
-                }else{
-                    while ((!s1.empty())&&!(getPriority(temp)>getPriority(s1.top()))) {
+                } else {
+                    while ((!s1.empty()) && !(getPriority(temp) > getPriority(s1.top()))) {
                         s2.push(s1.top());
                         s1.pop();
                     }
-                s1.push(temp);
+                    s1.push(temp);
                 }
             }
         }
@@ -94,49 +96,49 @@ int main() {
     string be;
     while (!s2.empty()) {
         be.insert(0, s2.top());
-        if (s2.size()!=1)
+        if (s2.size() != 1)
             be.insert(0, " ");
         s2.pop();
     }
-    bool sn=true;
-    for (int i=0; i<be.length(); i++) {
-        if (!isdigit(be[i])&&be[i]!='.') {
-            sn=false;
+    bool sn = true;
+    for (int i = 0; i < be.length(); i++) {
+        if (!isdigit(be[i]) && be[i] != '.') {
+            sn = false;
         }
     }
     if (sn) {
-        cout<<be<<endl;
-        #ifdef __WIN32
+        cout << be << endl;
+#ifdef __WIN32
         system("pause");
-        #endif
+#endif
         return 0;
     }
     stack<string> cs;
     string temp2;
-    long double val1=0,val2=0;
-    for (int i=0; i<be.length(); i++) {
-        temp2="";
+    long double val1 = 0, val2 = 0;
+    for (int i = 0; i < be.length(); i++) {
+        temp2 = "";
         if (isdigit(be[i])) {
-            while (be[i]!=' ') {
+            while (be[i] != ' ') {
                 temp2.push_back(be[i]);
                 i++;
             }
             cs.push(temp2);
-        }else{
-            val2=stold(cs.top());
+        } else {
+            val2 = stold(cs.top());
             cs.pop();
-            val1=stold(cs.top());
+            val1 = stold(cs.top());
             cs.pop();
-            if (be[i]=='+') {
-                cs.push(ldtos(val1+val2));
-            }else if (be[i]=='-') cs.push(ldtos(val1-val2));
-            else if (be[i]=='*') cs.push(ldtos(val1*val2));
-            else if (be[i]=='/') cs.push(ldtos((long double)val1/val2));
-            else cs.push(ldtos((long double)pow(val1, val2)));
+            if (be[i] == '+') {
+                cs.push(ldtos(val1 + val2));
+            } else if (be[i] == '-') cs.push(ldtos(val1 - val2));
+            else if (be[i] == '*') cs.push(ldtos(val1 * val2));
+            else if (be[i] == '/') cs.push(ldtos((long double) val1 / val2));
+            else cs.push(ldtos((long double) pow(val1, val2)));
             i++;
         }
     }
-    cout<<cs.top()<<endl;
+    cout << cs.top() << endl;
 #ifdef __WIN32
     system("pause");
 #endif
